6da34d701ef230981b49d90526c8a77060e04fb1
andre keedy <andre.keedy@emc.com>
2017-04-25 09:24:25 -0400

    change prefix to 2.0

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from flask import Flask
 from flask_restplus import Api
 
 app = Flask(__name__)
-api = Api(app, version='1.0', title='shovel API',
+api = Api(app, version='1.0', title='shovel API', prefix='/api/2.0',
           description='RackHD and OpenStack Coordinator')
